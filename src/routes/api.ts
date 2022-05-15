4a1016e599e364ed4924d1e2fc9e8f1faf3f860a
Nikolay-Jiang <gustywolf@gmail.com>
2022-05-15 18:40:03 +0800

    Update api.ts

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,5 +1,4 @@
 import { Router } from 'express';
-import { adminMw } from './middleware';
 import authRouter from './auth-router';
 import userRouter from './user-router';
 import mylistRouter from './mylist-router';
@@ -7,6 +6,8 @@ import observerRouter from './observer-router';
 import dayrptRouter from './dayrpt-router';
 import sinastockRouter from './sinastock-router';
 import daylogRouter from './daylog-router';
+import stocknameRouter from './stockname-router';
+import analysisRouter from './analysis-router';
 
 
 // Init
@@ -20,6 +21,8 @@ apiRouter.use('/mylist', mylistRouter);
 apiRouter.use('/dayrpt', dayrptRouter);
 apiRouter.use('/stock', sinastockRouter);
 apiRouter.use('/daylog', daylogRouter);
+apiRouter.use('/stockname', stocknameRouter);
+apiRouter.use('/analysis', analysisRouter);
 
 // Export default
 export default apiRouter;
