a9875ceb86534b1aaab4c5b5fa17b80a52e7360d
Nikolay-Jiang <gustywolf@gmail.com>
2022-05-17 15:04:48 +0800

    UpdateMA

--- src/routes/analysis-router.ts
+++ src/routes/analysis-router.ts
@@ -38,6 +38,7 @@ router.get(p.getbyconditicon, async (req: Request, res: Response) => {
 
     var RPMin: number = Number(dayrpts[0].RatePrice);
     var RPMax: number = Number(dayrpts[dayrpts.length - 1].RatePrice);
+    var MA: number = 0;
 
     var MaxDay: number = dayrpts.length;
     var rateanalysisdata: Array<rateAnalysis> = [];
@@ -46,13 +47,16 @@ router.get(p.getbyconditicon, async (req: Request, res: Response) => {
         rateanalysisdata[index] = new rateAnalysis();
         rateanalysisdata[index].maxday = MaxDay;
         rateanalysisdata[index].rateprice = Number(element.RatePrice);
-        rateanalysisdata[index].maxvalue = Number(element.RatePrice) * MaxDay;
-        rateanalysisdata[index].maxvalue.toFixed(2);
+        rateanalysisdata[index].maxvalue = Number((Number(element.RatePrice) * MaxDay).toFixed(2));
         rateanalysisdata[index].reportday = element.ReportDay;
         // console.log(element.RatePrice + "|" + MaxDay + "|" );
+        MA+=Number(element.TodayClosePrice);
+
         MaxDay--;
     }
     rateanalysisdata = rateanalysisdata.sort((a, b) => a.maxvalue - b.maxvalue);
+
+    MA=Number((MA/rateAnalysis.length).toFixed(2));
 
     var txtresult: string = "分析数据：\r\n"
     txtresult += "查询期内共有：" + dayrpts.length + "条日报数据\r\n";
@@ -60,7 +64,7 @@ router.get(p.getbyconditicon, async (req: Request, res: Response) => {
     txtresult += "最小振额：" + RPMin + ",日期：" + convertDatetoStr(dayrpts[0].ReportDay);
     txtresult += "最大振幅： " + RPMax + ",日期：" + convertDatetoStr(dayrpts[dayrpts.length - 1].ReportDay);
     txtresult += "\r\n最佳振幅： " + rateanalysisdata[dayrpts.length - 1].rateprice + " 计算值：" + rateanalysisdata[dayrpts.length - 1].maxday + "|" + rateanalysisdata[dayrpts.length - 1].maxvalue;
-    
+    txtresult += "MA:"+MA;
     return res.status(OK).json({ txtresult, rateanalysisdata });
 });
 
