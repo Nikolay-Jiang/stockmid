c167d3d12a49f028d40fd43ee54a283d568b090d
Nikolay-Jiang <gustywolf@gmail.com>
2022-08-12 14:20:40 +0800

    Update common-service.ts

--- src/services/common-service.ts
+++ src/services/common-service.ts
@@ -7,7 +7,7 @@ function checkCache():boolean {
     const used = process.memoryUsage().heapUsed / 1024 / 1024;
     var memUsed=Math.round(used * 100) / 100;
 
-    if (memUsed>3000) {return false;} //内存占用超过3G 不缓存
+    if (memUsed>1536) {return false;} //内存占用超过1.5G 不缓存 nodejs 最大内存2G...
     return true
 }
 
