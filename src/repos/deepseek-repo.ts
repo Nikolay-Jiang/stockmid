1b88555b19b96ddbb0aa884f794412f4d98cf27f
Nikolay-Jiang <gustywolf@gmail.com>
2026-02-28 11:21:03 +0800

    Update deepseek-repo.ts

--- src/repos/deepseek-repo.ts
+++ src/repos/deepseek-repo.ts
@@ -8,9 +8,11 @@ const openai = new OpenAI({
 async function callDeepSeek(prompt:string) {
   try {
     const completion = await openai.chat.completions.create({
-      messages: [{ role: "user", content: prompt }],
-      model: "deepseek-chat",
-      temperature:0.0,
+      messages: [{ role: "system", content: "你现在是一名商业数据分析师,你精通数据分析方法和工具，能够从大量数据中提取出有价值的商业洞察。你对业务运营有深入的理解，并能提供数据驱动的优化建议。请在这个角色下为我解答以下问题。" },{ role: "user", content: prompt }],
+      model: "qwen-plus",
+      temperature:0.2,
+      steam: false,
+      enable_search:true,
     });
 
     
